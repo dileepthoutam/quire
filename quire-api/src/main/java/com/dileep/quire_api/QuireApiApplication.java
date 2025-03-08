1e7c3fc0f55f01c6bf819db7ef96f882d3094366
dileepthoutam <thoutamdileep@gmail.com>
2025-03-08 20:55:48 +0530

    Initial commit

---
+++ quire-api/src/main/java/com/dileep/quire_api/QuireApiApplication.java
@@ -0,0 +1,13 @@
+package com.dileep.quire_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuireApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuireApiApplication.class, args);
+	}
+
+}
---
+++ quire-api/src/test/java/com/dileep/quire_api/QuireApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dileep.quire_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuireApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
